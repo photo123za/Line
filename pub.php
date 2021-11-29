d4689899420151bac123d5e7c379d114a4e0ad9e
photo123za <95198968+photo123za@users.noreply.github.com>
2021-11-29 12:06:36 +0700

    Update pub.php

--- pub.php
+++ pub.php
@@ -1,8 +1,8 @@
  <?php
  function pubMqtt($topic,$msg){
-       $APPID= "FixitCenter/"; //enter your appid
-     $KEY = "0ZcxhUOlSSrdtMt"; //enter your key
-    $SECRET = "JS9MqcQ2QyuujStrl7Y7Nqim2"; //enter your secret
+       $APPID= "photo"; //enter your appid
+     $KEY = "cY8jSlQ6xhEjAMT"; //enter your key
+    $SECRET = "n3x4HbFq5hnbXiK"; //enter your secret
     $Topic = "$topic"; 
       put("https://api.netpie.io/microgear/".$APPID.$Topic."?retain&auth=".$KEY.":".$SECRET,$msg);
  
